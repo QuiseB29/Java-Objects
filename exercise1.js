e341d91ec987aba58c628a740fc43cb6174a557d
QuiseB29 <160094863+QuiseB29@users.noreply.github.com>
2024-09-27 00:31:05 -0500

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -6,19 +6,39 @@ function Book(title,author,pages) {
     this.pages = pages;
 }
 
-function addBook(arr,books) {
-    arr.push(books);
-    return arr;
+// Function to add a new book to the library
+function addBook(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    library.push(newBook);
 }
 
-console.log(addBook(books, "Elves"))
+// Function to search books by title
+function searchByTitle(title) {
+    return library.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+}
 
+// Function to search books by author
+function searchByAuthor(author) {
+    return library.filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
+}
+
+function filterBooksByPages() {
+    return library.filter(book => book.pages > 100);
+}
+
+
+console.log(filterBooksByPages());
+console.log(addBook(books, "Elves"))
+console.log(searchByTitle('Hobbit'))
+console.log(filterBooksByPages());
+
+//display book info
 Book.prototype.displayInfo = function() {
     console.log("We are reading " + this.title + ", and it's fantastic");
 }
 
-let harryPotter = new Book("Harry Potter", ["J.k Rowling"], 400 );
-let hobbit = new Book("Hobbit", ["J.R.R Tolkien"], 350);
+let harryPotter = new Book("Harry Potter PT 2", ["J.k Rowling"], 400 );
+let hobbit = new Book("Hobbit Five Armies", ["J.R.R Tolkien"], 350);
 
 
 harryPotter.introduce()
